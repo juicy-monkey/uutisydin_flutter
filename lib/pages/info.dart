ac971c233a488899b1fb22fefea646e99c097d52
Jusa Mankki <jusamankki@gmail.com>
2025-04-02 19:43:26 +0300

    update info

--- lib/pages/info.dart
+++ lib/pages/info.dart
@@ -1,11 +1,19 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/gestures.dart';
+import 'package:url_launcher/url_launcher.dart';
 import 'package:uutisydin_flutter/components/juicy_monkey_link.dart';
 import 'package:uutisydin_flutter/utils/functions.dart';
 
 class Info extends StatelessWidget {
   const Info({super.key});
   final String version = '1.0.0';
+
+  Widget _buildUrlLinkButton(String label, String url) {
+    return TextButton(
+      onPressed: () => launchURL(url),
+      child: Text(label, style: const TextStyle(color: Colors.blue)),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -26,7 +34,8 @@ class Info extends StatelessWidget {
               Image.asset('assets/uutisydin/uutisydin_text.png', height: 25),
               SizedBox(height: 8),
               Text('Versio $version'),
-              SizedBox(height: 8),
+              Text('Kaikki oikeudet pidätetään'),
+              Text('All rights reserved'),
               JuicyMonkeyLink(),
               SizedBox(height: 20),
 
@@ -41,19 +50,19 @@ class Info extends StatelessWidget {
                   SizedBox(height: 8),
 
                   const Text(
-                    'Uutisydin on sovellus, joka hakee suomalaisista uutislähteistä uutiset ja kokoaa ne aiheen mukaan. '
-                    'Sovelluksen avulla voit seurata tärkeimpiä aiheita mistä on kirjoitettu viimeisen 48 tunnin ajalta. ',
+                    'Uutisydin on sovellus, joka kokoaa suomalaisista uutislähteistä ajankohtaisimmat uutiset ja ryhmittelee ne aiheittain. '
+                    'Sovelluksen avulla pysyt helposti ajan tasalla tärkeimmistä tapahtumista viimeisen 48 tunnin ajalta. ',
                   ),
                   SizedBox(height: 8),
 
                   const Text(
-                    'Voit hakea hakutoiminnolla sisältöä, ja järjestää sisällön uusimpien julkaisujen mukaan, tai aiheiden mukaan joista on eniten kirjoitettu. '
-                    'Uutiskoosteita päivitetään 15 minuutin välein. ',
+                    'Voit hakea uutisia hakutoiminnolla sekä järjestää ne joko julkaisuajan mukaan tai aiheiden mukaan, joista on kirjoitettu eniten. '
+                    'Uutiskoosteet päivittyvät automaattisesti 15 minuutin välein. ',
                   ),
                   SizedBox(height: 8),
 
                   const Text(
-                    'Sovellus ei kerää mitään käyttäjäkohtaista tietoa. ',
+                    'Sovellus ei kerää käyttäjäkohtaista tietoa. ',
                   ),
                   SizedBox(height: 8),
 
@@ -66,9 +75,7 @@ class Info extends StatelessWidget {
                         ),
                         TextSpan(
                           text: 'sähköpostitse. ',
-                          style: const TextStyle(
-                            color: Colors.blue,
-                          ),
+                          style: const TextStyle(color: Colors.blue),
                           recognizer:
                               TapGestureRecognizer()
                                 ..onTap = () {
@@ -91,36 +98,72 @@ class Info extends StatelessWidget {
                   SizedBox(height: 8),
 
                   const Text(
-                    'Uutiset on kerätty uutistoimitusten RSS-syötteistä. '
-                    'Uutisydin sovellus ei ole tehnyt yhteistyötä uutistoimitusten kanssa. '
-                    'On katsottu, että se on sovellus noudattaa RSS-syötteiden käyttöehtoja ja kunnioittaa uutistoimitusten tekijänoikeussuojaa. ',
+                    'Uutiset, joita sovellus näyttää, on haettu suoraan uutistoimitusten julkisista RSS-syötteistä. '
+                    'Uutisydin-sovellus toimii itsenäisesti, eikä sillä ole virallista yhteistyötä uutistoimitusten kanssa. '
+                    'Sovellus noudattaa RSS-syötteiden käyttöehtoja ja kunnioittaa sisällöntuottajien tekijänoikeuksia. '
+                    'Kaikki linkit ohjaavat suoraan alkuperäisiin uutisartikkeleihin, jotta käyttäjät voivat lukea uutiset luotettavasta lähteestä. ',
                   ),
                   SizedBox(height: 8),
 
                   const Text(
-                    'Kaikki sovelluksessa käytetyt kuvat ovat CC-BY lisenssillä. ',
+                    'Sovellus hyödyntää seuraavien uutistoimitusten RSS-syötteitä: ',
+                  ),
+                  Wrap(
+                    children: [
+                      _buildUrlLinkButton(
+                        'Helsingin Sanomat',
+                        'https://www.hs.fi',
+                      ),
+                      _buildUrlLinkButton(
+                        'Iltalehti',
+                        'https://www.iltalehti.fi',
+                      ),
+                      _buildUrlLinkButton('Ilta-Sanomat', 'https://www.is.fi'),
+                      _buildUrlLinkButton('Kaleva', 'https://www.kaleva.fi'),
+                      _buildUrlLinkButton(
+                        'Kauppalehti',
+                        'https://www.kauppalehti.fi',
+                      ),
+                      _buildUrlLinkButton('Turun Sanomat', 'https://www.ts.fi'),
+                      _buildUrlLinkButton('Yle', 'https://www.yle.fi'),
+                    ],
+                  ),
+                  SizedBox(height: 20),
+
+                  //////////////////////////////
+                  const Text(
+                    'Lisenssit',
+                    style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+                  ),
+                  SizedBox(height: 8),
+
+                  TextButton(
+                    onPressed: () {
+                      launchURL('https://juicy-monkey.github.io/uutisydin_node/image-licences.html');
+                    },
+                    child: const Text(
+                      'Kuvien lisenssit',
+                      style: TextStyle(color: Colors.blue),
+                    ),
+                  ),
+                  SizedBox(height: 8),
+
+                  TextButton(
+                    onPressed: () {
+                      showLicensePage(
+                        context: context,
+                        applicationName: 'Uutisydin',
+                        applicationVersion: version,
+                      );
+                    },
+                    child: const Text(
+                      'Flutter lisenssit',
+                      style: TextStyle(color: Colors.blue),
+                    ),
                   ),
                   SizedBox(height: 8),
                 ],
               ),
-
-              //////////////////////////////
-              TextButton(
-                onPressed: () {
-                  showLicensePage(
-                    context: context,
-                    applicationName: 'Uutisydin',
-                    applicationVersion: version,
-                  );
-                },
-                child: const Text(
-                  'Lisenssit',
-                  style: TextStyle(
-                    color: Colors.blue,
-                  ),
-                ),
-              ),
-              SizedBox(height: 8),
             ],
           ),
         ),

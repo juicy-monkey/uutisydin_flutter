a0c458edd242b230bd61f3f4bf7562d27d639ba3
Jusa Mankki <jusamankki@gmail.com>
2025-04-02 11:20:46 +0300

    add title img and version

--- lib/pages/info.dart
+++ lib/pages/info.dart
@@ -13,22 +13,30 @@ class Info extends StatelessWidget {
             Navigator.pop(context);
           },
         ),
-        title: const Text('Tietoa'),
       ),
-      body: const Padding(
-        padding: EdgeInsets.all(16.0),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
         child: SingleChildScrollView(
-          child: Text(
-            'Lorem ipsum dolor sit amet, consectetur adipiscing elit. '
-            'Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. '
-            'Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi '
-            'ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit '
-            'in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint '
-            'occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim '
-            'id est laborum. Lorem ipsum dolor sit amet, consectetur adipiscing elit. '
-            'Vivamus luctus urna sed urna ultricies ac tempor dui sagittis. In condimentum facilisis porta. '
-            'Sed nec diam eu diam mattis viverra. Nulla fringilla, orci ac euismod semper, magna diam porttitor mauris.',
-            style: TextStyle(fontSize: 16),
+          child: Column(
+            children: [
+              Image.asset('assets/uutisydin/uutisydin_text.png', height: 25),
+              SizedBox(height: 8),
+              const Text('Versio 1.0.0'),
+              SizedBox(height: 20),
+
+              const Text(
+                'Lorem ipsum dolor sit amet, consectetur adipiscing elit. '
+                'Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. '
+                'Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi '
+                'ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit '
+                'in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint '
+                'occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim '
+                'id est laborum. Lorem ipsum dolor sit amet, consectetur adipiscing elit. '
+                'Vivamus luctus urna sed urna ultricies ac tempor dui sagittis. In condimentum facilisis porta. '
+                'Sed nec diam eu diam mattis viverra. Nulla fringilla, orci ac euismod semper, magna diam porttitor mauris.',
+                style: TextStyle(fontSize: 16),
+              ),
+            ],
           ),
         ),
       ),

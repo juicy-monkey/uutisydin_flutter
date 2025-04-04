427168ff399278ce82764cd0359cda67398dd97c
Jusa Mankki <jusamankki@gmail.com>
2025-04-04 09:05:35 +0300

    fix email subject header

--- lib/utils/functions.dart
+++ lib/utils/functions.dart
@@ -1,11 +1,9 @@
 import 'package:url_launcher/url_launcher.dart';
 
 void launchEmail(String subject) async {
-  final Uri emailUri = Uri(
-    scheme: 'mailto',
-    path: 'jusamankki@gmail.com',
-    queryParameters: {'subject': subject, 'body': ''},
-  );
+  final String email = 'jusamankki@gmail.com';
+  final String encodedSubject = Uri.encodeComponent(subject);
+  final Uri emailUri = Uri.parse('mailto:$email?subject=$encodedSubject');
 
   await launchUrl(emailUri);
 }

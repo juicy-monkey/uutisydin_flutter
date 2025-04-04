ffae5dcbfa0bd9a2678e02b7e9abb5826733869d
Jusa Mankki <jusamankki@gmail.com>
2025-04-04 17:20:05 +0300

    make whole card clickable

--- lib/components/news_card_dialog_item.dart
+++ lib/components/news_card_dialog_item.dart
@@ -13,102 +13,108 @@ class NewsCardDialogItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Card(
-      color: Colors.white,
-      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(12.0)),
-      elevation: 1.0,
-      child: Padding(
-        padding: const EdgeInsets.all(12.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Row(
-              children: [
-                Container(
-                  width: 10,
-                  height: 10,
-                  decoration: BoxDecoration(
-                    shape: BoxShape.circle,
-                    boxShadow: [
-                      BoxShadow(
-                        color: const Color.fromARGB(255, 72, 72, 72),
-                        blurRadius: 1,
-                        offset: Offset(0.1, 1),
-                      ),
-                    ],
-                  ),
-                  child: ClipOval(
-                    child: Image.asset(
-                      'assets/logos/${news.publisherId}.png',
-                      fit: BoxFit.cover,
-                    ),
-                  ),
-                ),
-                const SizedBox(width: 6.0),
-                Text(
-                  news.publisher,
-                  style: const TextStyle(
-                    fontSize: 12,
-                    color: Color.fromARGB(255, 131, 131, 131),
-                  ),
-                ),
-              ],
-            ),
-
-            const SizedBox(height: 6.0),
-            Row(
-              children: [
-                Expanded(
-                  child: Text(
-                    news.title,
-                    style: const TextStyle(
-                      fontSize: 16,
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                ),
-              ],
-            ),
-            const SizedBox(height: 6.0),
-
-            if (news.content.isNotEmpty)
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
+    return InkWell(
+      onTap: () {
+        launchUrl(Uri.parse(news.link));
+      },
+      child: Card(
+        color: Colors.white,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(12.0),
+        ),
+        elevation: 1.0,
+        child: Padding(
+          padding: const EdgeInsets.all(12.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Row(
                 children: [
-                  Text(news.content, style: const TextStyle(fontSize: 14)),
-                  const SizedBox(height: 10.0),
+                  Container(
+                    width: 10,
+                    height: 10,
+                    decoration: BoxDecoration(
+                      shape: BoxShape.circle,
+                      boxShadow: [
+                        BoxShadow(
+                          color: const Color.fromARGB(255, 72, 72, 72),
+                          blurRadius: 1,
+                          offset: Offset(0.1, 1),
+                        ),
+                      ],
+                    ),
+                    child: ClipOval(
+                      child: Image.asset(
+                        'assets/logos/${news.publisherId}.png',
+                        fit: BoxFit.cover,
+                      ),
+                    ),
+                  ),
+                  const SizedBox(width: 6.0),
+                  Text(
+                    news.publisher,
+                    style: const TextStyle(
+                      fontSize: 12,
+                      color: Color.fromARGB(255, 131, 131, 131),
+                    ),
+                  ),
                 ],
               ),
 
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Text(
-                  timeString,
-                  style: const TextStyle(fontSize: 12, color: Colors.grey),
-                ),
-                InkWell(
-                  onTap: () {
-                    launchUrl(Uri.parse(news.link));
-                  },
-                  child: Row(
-                    children: [
-                      const Icon(Icons.link, size: 16, color: Colors.blue),
-                      const SizedBox(width: 4),
-                      Text(
-                        news.publisherUrl,
-                        style: TextStyle(
-                          fontSize: 14,
-                          color: Theme.of(context).primaryColor,
-                          decoration: TextDecoration.underline,
-                        ),
+              const SizedBox(height: 6.0),
+              Row(
+                children: [
+                  Expanded(
+                    child: Text(
+                      news.title,
+                      style: const TextStyle(
+                        fontSize: 16,
+                        fontWeight: FontWeight.bold,
                       ),
-                    ],
+                    ),
                   ),
+                ],
+              ),
+              const SizedBox(height: 6.0),
+
+              if (news.content.isNotEmpty)
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(news.content, style: const TextStyle(fontSize: 14)),
+                    const SizedBox(height: 10.0),
+                  ],
                 ),
-              ],
-            ),
-          ],
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                    timeString,
+                    style: const TextStyle(fontSize: 12, color: Colors.grey),
+                  ),
+                  InkWell(
+                    onTap: () {
+                      launchUrl(Uri.parse(news.link));
+                    },
+                    child: Row(
+                      children: [
+                        const Icon(Icons.link, size: 16, color: Colors.blue),
+                        const SizedBox(width: 4),
+                        Text(
+                          news.publisherUrl,
+                          style: TextStyle(
+                            fontSize: 14,
+                            color: Theme.of(context).primaryColor,
+                            decoration: TextDecoration.underline,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
         ),
       ),
     );
